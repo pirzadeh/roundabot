f9905bddc87925a4538f223978e51f5401a56f6d
Amir <pirzadeh@gmail.com>
2016-04-16 23:13:57 -0400

    added the client info

--- app/src/main/java/com/infobion/roundabot/ApplicationData.java
+++ app/src/main/java/com/infobion/roundabot/ApplicationData.java
@@ -1,7 +1,7 @@
 package com.infobion.roundabot;
 
 public class ApplicationData {
-	public static final String CLIENT_ID = "";
-	public static final String CLIENT_SECRET = "";
-	public static final String CALLBACK_URL = "";
+	public static final String CLIENT_ID = "95716ce8e73a4431b9e52421506675b1";
+	public static final String CLIENT_SECRET = "35d20e221818455c81aa68afa75e7024";
+	public static final String CALLBACK_URL = "roundabot://redirect";
 }
